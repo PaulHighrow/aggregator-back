fd7595e2048b5df422919d04fff8565578fea448
Kyrylo Sulihan <106202828+PaulHighrow@users.noreply.github.com>
2023-09-26 15:38:28 +0300

    refresh upd

--- middlewares/crm/crmRefresh.js
+++ middlewares/crm/crmRefresh.js
@@ -1,28 +1,20 @@
 const axios = require("axios");
-const fs = require('fs').promises;
-const path = require("path");
-const tokenPath = path.join(__dirname, "../../utils/crm/tokens.json")
-const token = require("../../utils/crm/tokens.json");
+const { getToken } = require("../../services/tokensServices");
 
 axios.defaults.baseURL = "https://apeducation.kommo.com/";
 
-const refreshRequest = {
-  client_id: "d7412514-d863-4a1b-b396-92b243063b65",
-  client_secret:
-    "nWi0Cg6ffpzpAK65WuvtyJm2qp6sBfVusMm86pPTlcTQDWa9YUxmYqNOB0bBO57q",
-  grant_type: "refresh_token",
-  refresh_token: `${token.refresh_token}`,
-  redirect_uri: "https://paulhighrow.github.io/aggregator/",
-};
-
 const crmRefresh = async (_, __, next) => {
   try {
-    const refreshResp = await axios.post(
-      "oauth2/access_token",
-      refreshRequest
-    );
-    console.log(refreshResp.data);
-    await fs.writeFile(tokenPath, JSON.stringify(refreshResp.data), "utf-8");
+    const currentToken = getToken();
+    console.log(currentToken);
+    if (currentToken) {
+      const refreshResp = await axios.post(
+        "oauth2/access_token", currentToken
+      );
+      console.log(refreshResp);
+      console.log(refreshResp.data);
+    }
+
     next();
   } catch (error) {
     console.log(error.response.data);
--- services/tokensServices.js
+++ services/tokensServices.js
@@ -2,7 +2,7 @@ const Tokens = require("../db/models/tokensModel");
 
 const savePrimaryToken = async (body) => await Tokens(body).save();
 
-const getToken = async (id) => await Tokens.findById(id);
+const getToken = async () => await Tokens.find({});
 
 const refreshToken = async (id, update) =>
   await Tokens.findByIdAndUpdate(id, update, { new: true });
