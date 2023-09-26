6d5887dd65e54d695a0f1f1c845feca882a8030d
Kyrylo Sulihan <106202828+PaulHighrow@users.noreply.github.com>
2023-09-26 15:43:31 +0300

    refresh and auth upd

--- middlewares/crm/crmAuth.js
+++ middlewares/crm/crmAuth.js
@@ -14,9 +14,14 @@ const authRequest = {
 
 const crmAuth = async (_, __, next) => {
   try {
-    const authResp = await axios.post("oauth2/access_token", authRequest);
-    console.log(authResp.data);
-    await savePrimaryToken(authResp.data);
+    const currentToken = getToken();
+    console.log(currentToken);
+    if (!currentToken) {
+      const authResp = await axios.post("oauth2/access_token", authRequest);
+      console.log(authResp.data);
+      await savePrimaryToken(authResp.data);
+      next();
+    }
     next();
   } catch (error) {
     console.log(error.response.data);
--- routes/tokens.js
+++ routes/tokens.js
@@ -1,9 +1,10 @@
 const express = require("express");
 
 const crmAuth = require("../middlewares/crm/crmAuth");
+const crmRefresh = require("../middlewares/crm/crmRefresh");
 
 const router = express.Router();
 
-router.post("/", crmAuth);
+router.post("/", crmRefresh, crmAuth);
 
 module.exports = router;
