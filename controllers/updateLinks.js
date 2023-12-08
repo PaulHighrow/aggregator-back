c03eee2a5a5c45e386ad3f72d5a3702378477b1d
Kyrylo Sulihan <paulhighrow@gmail.com>
2023-12-08 21:25:53 +0200

    updateLinks controller update

--- controllers/updateLinks.js
+++ controllers/updateLinks.js
@@ -2,8 +2,14 @@ const Links = require("../db/models/linksModel");
 const { patchLinks } = require("../services/linksServices");
 
 const updateLinks = async (req, res) => {
-  const id = await Links.findOne({});
-  console.log(id);
-  res.status(201).json(await patchLinks(id, req.body));
+  const links = await Links.findOne({});
+  const body = {};
+  for (const [key, value] of Object.entries(req.body)) {
+    if (value) {
+      body[key] = value;
+    }
+  }
+  console.log(body);
+  res.status(201).json(await patchLinks(links.id, body));
 };
 module.exports = updateLinks;
