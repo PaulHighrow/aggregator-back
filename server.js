f5811ba55a5a997af32106c07689a707ab9ee404
Kyrylo Sulihan <paulhighrow@gmail.com>
2023-12-22 20:23:26 +0200

    chat server added

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const app = require('./app')
+const { app, chatServer } = require("./app");
 const connectDB = require("./db/connection");
 require("dotenv").config();
 
@@ -13,6 +13,17 @@ const startServer = async () => {
     });
   } catch (err) {
     console.log(`Failed to launch application with an error: "${err.message}"`);
+    process.exit(1);
+  }
+  try {
+    chatServer.listen(process.env.CHAT_PORT, (error) => {
+      if (error) {
+        console.log("Server launch error", error);
+      }
+      console.log(`Chat on port ${process.env.CHAT_PORT}`);
+    });
+  } catch (err) {
+    console.log(`Failed to launch chat server with an error: "${err.message}"`);
     process.exit(1);
   }
 };
