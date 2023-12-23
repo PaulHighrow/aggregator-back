b3d0b61a69a76a9bba21a40327bb37257e7eac6f
Kyrylo Sulihan <106202828+PaulHighrow@users.noreply.github.com>
2023-12-23 10:28:52 +0200

    separated chat server

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const { app, chatServer } = require("./app");
+const app = require("./app");
 const connectDB = require("./db/connection");
 require("dotenv").config();
 
@@ -13,17 +13,6 @@ const startServer = async () => {
     });
   } catch (err) {
     console.log(`Failed to launch application with an error: "${err.message}"`);
-    process.exit(1);
-  }
-  try {
-    chatServer.listen(process.env.CHAT_PORT, (error) => {
-      if (error) {
-        console.log("Server launch error", error);
-      }
-      console.log(`Chat on port ${process.env.CHAT_PORT}`);
-    });
-  } catch (err) {
-    console.log(`Failed to launch chat server with an error: "${err.message}"`);
     process.exit(1);
   }
 };
