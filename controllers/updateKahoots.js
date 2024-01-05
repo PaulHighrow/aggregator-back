83835435cb685e95d6031ae6a8fbe69a1eca8942
Kyrylo Sulihan <paulhighrow@gmail.com>
2024-01-05 20:04:23 +0200

    replace and edit kahoots function

--- controllers/updateKahoots.js
+++ controllers/updateKahoots.js
@@ -3,20 +3,16 @@ const { patchKahoots } = require("../services/kahootsServices");
 
 const updateKahoots = async (req, res) => {
   const kahoots = await Kahoots.findOne({});
-  console.log(kahoots);
   const body = {};
   for (const [key, value] of Object.entries(req.body)) {
-    console.log(value.replace);
-    console.log(key);
-    console.log(value);
     if (!value.replace) {
-      console.log(body);
+      body[key] = { ...kahoots[key] };
+      body[key].links = { ...body[key].links, ...value.links };
     }
-    if (value) {
+    if (value && value.replace) {
       body[key] = value;
     }
   }
-  console.log(body);
   res.status(201).json(await patchKahoots(kahoots.id, body));
 };
 module.exports = updateKahoots;
