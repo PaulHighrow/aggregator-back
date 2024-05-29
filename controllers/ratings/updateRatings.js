a299a6b3281759a9024b546ccefee35856ae2758
Kyrylo Sulihan <paulhighrow@gmail.com>
2024-05-29 15:58:12 +0300

    rating update route upgrade

--- controllers/ratings/updateRatings.js
+++ controllers/ratings/updateRatings.js
@@ -2,7 +2,7 @@ const Ratings = require("../../db/models/ratingsModel");
 const { patchRatings } = require("../../services/ratingsServices");
 
 const updateRatings = async (req, res) => {
-  const ratings = await Ratings.findOne({});
+  const ratings = await Ratings.findOne({ period: req.body.period });
   const body = {};
   for (const [key, value] of Object.entries(req.body)) {
     if (value) {
